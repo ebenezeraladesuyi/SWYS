39f44b5e6c1d8ac93b270909245723e19f28fec4
ebenezeraladesuyi <ebenezeraladesuyi@gmail.com>
2024-01-24 22:55:22 +0100

    new

--- src/components/static/Countdown.tsx
+++ src/components/static/Countdown.tsx
@@ -36,7 +36,7 @@ const Countdown: React.FC<CountdownProps> = ({ targetDate }) => {
         <div className="w-[21%] md:w-[15%] h-[80px] md:h-[110px] border-[1px] rounded border-white flex flex-col justify-center items-center gap-2 text-white lg:w-[12%] lg:h-[120px]">
             <h5 className="h-[60%] font-semibold text-[30px] md:text-[45px] lg:text-[55px]">{timeLeft.days}</h5>
             <hr className="w-full border-[1px] border-white"/>
-            <h5 className="font-semibold text-[10px] md:text-[14px] lg:text-[17px]">DAYS</h5>
+            <h5 className="font-semibold text-[10px] md:text-[14px] lg:text-[17px]">DAY</h5>
         </div>
 
         <div className="w-[21%] md:w-[15%] h-[80px] md:h-[110px] border-[1px] rounded border-white flex flex-col justify-center items-center gap-2 text-white lg:w-[12%] lg:h-[120px]">
