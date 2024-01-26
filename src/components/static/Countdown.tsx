850ad02de10ac2f5f8bd5fc77dfea7fe9b1a0c10
ebenezeraladesuyi <ebenezeraladesuyi@gmail.com>
2024-01-26 11:23:12 +0100

    new

--- src/components/static/Countdown.tsx
+++ src/components/static/Countdown.tsx
@@ -21,6 +21,7 @@ const Countdown: React.FC<CountdownProps> = ({ targetDate }) => {
   };
 
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft);
+  console.log(timeLeft)
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -34,25 +35,37 @@ const Countdown: React.FC<CountdownProps> = ({ targetDate }) => {
   return (
     <div className="w-full flex gap-3 md:gap-4 lg:gap-7 items-center justify-center">
         <div className="w-[21%] md:w-[15%] h-[80px] md:h-[110px] border-[1px] rounded border-white flex flex-col justify-center items-center gap-2 text-white lg:w-[12%] lg:h-[120px]">
-            <h5 className="h-[60%] font-semibold text-[30px] md:text-[45px] lg:text-[55px]">{timeLeft.days}</h5>
+            <h5 className="h-[60%] font-semibold text-[30px] md:text-[45px] lg:text-[55px]">
+              {/* {timeLeft.days} */}
+              0
+            </h5>
             <hr className="w-full border-[1px] border-white"/>
             <h5 className="font-semibold text-[10px] md:text-[14px] lg:text-[17px]">DAY</h5>
         </div>
 
         <div className="w-[21%] md:w-[15%] h-[80px] md:h-[110px] border-[1px] rounded border-white flex flex-col justify-center items-center gap-2 text-white lg:w-[12%] lg:h-[120px]">
-            <h5 className="h-[60%] font-semibold text-[30px] md:text-[45px] lg:text-[55px]">{timeLeft.hours}</h5>
+            <h5 className="h-[60%] font-semibold text-[30px] md:text-[45px] lg:text-[55px]">
+              {/* {timeLeft.hours} */}
+              0
+              </h5>
             <hr className="w-full border-[1px] border-white"/>
             <h5 className="font-semibold text-[10px] md:text-[14px] lg:text-[17px]">HOURS</h5>
         </div>
 
         <div className="w-[21%] md:w-[15%] h-[80px] md:h-[110px] border-[1px] rounded border-white flex flex-col justify-center items-center gap-2 text-white lg:w-[12%] lg:h-[120px]">
-            <h5 className="h-[60%] font-semibold text-[30px] md:text-[45px] lg:text-[55px]">{timeLeft.minutes}</h5>
+            <h5 className="h-[60%] font-semibold text-[30px] md:text-[45px] lg:text-[55px]">
+              {/* {timeLeft.minutes} */}
+              0
+              </h5>
             <hr className="w-full border-[1px] border-white"/>
             <h5 className="font-semibold text-[10px] md:text-[14px] lg:text-[17px]">MINUTES</h5>
         </div>
 
         <div className="w-[21%] md:w-[15%] h-[80px] md:h-[110px] border-[1px] rounded border-white flex flex-col justify-center items-center gap-2 text-white lg:w-[12%] lg:h-[120px]">
-            <h5 className="h-[60%] font-semibold text-[30px] md:text-[45px] lg:text-[55px]">{timeLeft.seconds}</h5>
+            <h5 className="h-[60%] font-semibold text-[30px] md:text-[45px] lg:text-[55px]">
+              {/* {timeLeft.seconds} */}
+              0
+              </h5>
             <hr className="w-full border-[1px] border-white"/>
             <h5 className="font-semibold text-[10px] md:text-[14px] lg:text-[17px]">SECONDS</h5>
         </div>
